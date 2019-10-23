2e368a7216ae29e07b4674433bd851495ffa0782
eddie_yang <star7885v@hotmail.com>
2019-10-23 23:07:15 +0800

    clear

--- profile/js/all.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.scrolltop').click(function (event) {
-        event.preventDefault();
-        $('html,body').animate({
-            scrollTop: 0
-        }, 300);
-    });
-});
