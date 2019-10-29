2ac0af6eee3cf9090ea62068c37510b77839a76f
eddie_yang <star7885v@hotmail.com>
2019-10-29 09:49:28 +0800

    changet2

--- works/kaotravel/js/all.js
+++ works/kaotravel/js/all.js
@@ -1,8 +1,8 @@
 var data;
 var xhr = new XMLHttpRequest();
-xhr.open('post', 'https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97', true);
-xhr.setRequestHeader('Content-type', 'application/json');
-xhr.send();
+xhr.open('get', 'https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97', true);
+// xhr.setRequestHeader('Content-type', 'application/json');
+xhr.send(null);
 xhr.onload = function () {    
     data = JSON.parse(xhr.responseText).result.records;
 
